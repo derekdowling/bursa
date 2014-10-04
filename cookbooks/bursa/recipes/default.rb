2647f862ae2458f38a006e98fe2f0a1694b31d2f
derekdowling <thederekdowling@gmail.com>
2014-10-03 18:05:40 -0600

    Fixing default attributes path

--- cookbooks/bursa/recipes/default.rb
+++ cookbooks/bursa/recipes/default.rb
@@ -7,17 +7,14 @@
 include_recipe "golang"
 include_recipe "bursa-gpm"
 
-default["bursa"]["path"] = "/bursa"
-default["bursa"]["gopath"] = "#{default["bursa"]["path"]}/app"
-
 env "GOROOT" do
     action :create
-    value "#{default["bursa"]["path"]}"
+    value node["bursa"]["path"]
 end
 
 # Appends the value to the end with the delimiter in between
 env "PATH" do
     action :modify
-    value "#{default["bursa"]["path"]}/bin"
+    value "#{node["bursa"]["path"]}/bin"
     delim ":"
 end
