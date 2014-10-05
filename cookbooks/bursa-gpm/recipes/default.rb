2b9a99b815a1aa0104391dd00da04b6a0be5ea95
derekdowling <thederekdowling@gmail.com>
2014-10-05 14:28:09 -0600

    Adding gvp

--- cookbooks/bursa-gpm/recipes/default.rb
+++ cookbooks/bursa-gpm/recipes/default.rb
@@ -6,5 +6,10 @@
 
 remote_file "/usr/local/bin/gpm" do
   source "https://raw.githubusercontent.com/pote/gpm/v1.3.1/bin/gpm"
-  mode 0777
+  mode 0755
+end
+
+remote_file "/usr/local/bin/gvp" do
+  source "https://raw.githubusercontent.com/pote/gvp/master/bin/gvp"
+  mode 0755
 end
