eac514ab92900ae0e96f67504922689904c91b59
derekdowling <thederekdowling@gmail.com>
2014-10-05 15:12:05 -0600

    Adding ruby and bundler to chef

--- cookbooks/bursa/recipes/default.rb
+++ cookbooks/bursa/recipes/default.rb
@@ -7,6 +7,7 @@
 include_recipe "golang"
 include_recipe "bursa-gpm"
 include_recipe "bursa-duojs"
+include_recipe "bursa::ruby"
 
 # FUTURE ELASTICSEARCH SERVER
 # Until we start provisioning different types of production servers, this will be
