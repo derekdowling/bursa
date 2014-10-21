edfe5d6b6a26b449876875578ace79187d3faa3c
jacobstr <jacobstr@gmail.com>
2014-10-21 02:36:01 -0600

    Hopefully starting to see transactions soon.
    Some magical stuff has to happen first. We generate bitcoins in bitcoind - which
    end up being part of whatever default account it provisions itself with.
    
    Then we need to use raw transactions to generate the transfer - otherwise it
    assuems the source of the money is bitcoind wallet - which we should never ever
    use for our users.

--- src/bursa.io/latinum/backend/bitcoin.go
+++ src/bursa.io/latinum/backend/bitcoin.go
@@ -3,8 +3,11 @@ package backend
 import (
 	"log"
 
+	"bursa.io/latinum/shared/config"
+	"bursa.io/latinum/vault"
 	"bursa.io/config"
 	"github.com/conformal/btcrpcclient"
+	"github.com/conformal/btcjson"
 	"github.com/spf13/viper"
 )
 
@@ -62,9 +65,67 @@ func (self *Latinum) Send(from_address string, to_adddress string, amt int) {
 
 // Generates bitcoins and gives them to an address. Used only during testing
 // in regtest mode where setgenerate is actually functional.
-func (self *Latinum) GenerateInto(amt int, address string) error {
+//
+// When we generate our initial bitcoins for test purposes, they're implicitly
+// associated with bitcoind's default wallet. Generally, all of Bursa's other
+// operations take the burden of wallet management out of bitcoind.
+//
+// Amt shoult be set to > 101 in order to ensure we've confirmed some blocks.
+// to make the newly minted bitcoins spendable. Yes - we're "mining" here. The
+// 100 confirmations thing is to iron out blockchain forks.
+func (self *Latinum) GenerateInto(amt int, encoded_address string) error {
 	err := self.client.SetGenerate(true, amt)
 	if err != nil {
 		log.Fatalf("Couldn't generate bitcoins")
 	}
+
+	unspent, err := self.client.ListUnspent()
+
+	// Aggregate unspent transactions until we have more than then requested amount.
+	// Who needs ruby? Good old for loops.
+	current_amt := 0
+	index := 0
+	var inputs []btcjson.TransactionInput
+	for utxo := range unspent {
+		if current_amt > amt {
+			break
+		}
+
+		current_amt += utxo.amount
+		append(btcjson.TransactionInput{Txid: utxo.Txid, Vout: index}, utxo)
+		index += 1
+	}
+
+	if (curent_amt < amt) {
+		return errors.New("Insufficient funds in server wallet")
+	}
+
+	// Transaction fee is the difference between in/out.
+	tx_fee := 0.00001
+
+	// Calculate change to send back to ourselves.
+	change := current_amt - tx_fee - amt
+
+	var inputs []btcjson.TransactionInput
+	var amounts map[btcutil.Address]btcutil.Amount
+
+	src_address, err := self.client.GetNewAddress()
+	if err != nil {
+		return err
+	}
+
+	dest_address, err := btcutil.DecodeAddress(encoded_address, config.BTCNet())
+
+	amounts[src_address] = amt
+	amounts[dest_address] = change
+
+	unsigned_raw_tx, err := self.client.CreateRawTransaction(inputs, amounts)
+
+	// TODO we may want to return a new error rather than the descended one.
+	if err != nil {
+		return err
+	}
+
+	// This should be around the final step.
+	// self.latinum.client.SignRawTransaction(unsigned_raw_tx, 
 }
--- src/bursa.io/latinum/vault/vault.go
+++ src/bursa.io/latinum/vault/vault.go
@@ -8,26 +8,9 @@ package vault
 
 import (
 	"bursa.io/latinum/vault/store"
-	"github.com/conformal/btcnet"
 	"github.com/conformal/btcutil/hdkeychain"
 	"log"
 )
-
-// Network parameters are necessary during the derivation of public keys
-// from extended keys. They vary between mainnet and regtest.
-// TOLEARN Which parameters exactly are used during the derivation? How do they
-// affect things?
-//
-// TODO This flag being wrong in production could represent a signficant risk.
-// Would money be lost if we tried to sign transfers using bunk keys? Or would
-// they simply "bounce"?
-var btc_network *btcnet.Params
-
-func init() {
-	// TODO PROD
-	// btc_network = &btcnet.MainNetParams
-	btc_network = &btcnet.RegressionNetParams
-}
 
 func GetPublicKey(username string) string {
 	return "Stub"
