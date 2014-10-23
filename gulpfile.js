e37bb352e51590b512da52adb23744a127c3112a
derekdowling <thederekdowling@gmail.com>
2014-10-22 23:39:40 -0600

    Cleaning up ignores, building static assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
 var gulp    = require('gulp')
 , shell     = require('gulp-shell');
 
-server_path = "src/bursa.io/server";
-static_path = server_path + '/static/';
-
 gulp.task('duo', shell.task([
-    'duo ' + server_path + '/assets/build.js > ' + static_path + '/js/build.js',
-    'duo --use duosass ' + server_path + '/assets/build.scss > ' + static_path + '/css/build.css'
+    'duo assets/build.js > static/js/build.js',
+    'duo --use duosass assets/build.scss > static/css/build.css'
 ]));
 
 gulp.task('cjsx', shell.task([
