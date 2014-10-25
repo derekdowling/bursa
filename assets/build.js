669418cacd13cfd333125caab9c713c195eef14f
derekdowling <thederekdowling@gmail.com>
2014-10-24 22:06:45 -0600

    restructuring code

--- assets/build.js
+++
@@ -1,5 +0,0 @@
-var react = require("facebook/react:main.js");
-var bootstrap  = require("twbs/bootstrap:dist/js/bootstrap.min.js");
-var coffee-react = require("jsdf/coffee-react-transform");
-var jquery = require("jquery/jquery:dist/jquery.js");
-var boostrap-validate = require("nghuuphuoc/bootstrapvalidator:dist/js/boostrapValidator.js");
---
+++ assets/coffee/base.coffee
@@ -0,0 +1,4 @@
+jquery          = require "jquery/jquery:dist/jquery.js"
+react           = require "facebook/react:main.js"
+bootstrap       = require "twbs/bootstrap:dist/js/bootstrap.min.js"
+coffee_react    = require "jsdf/coffee-react-transform"
