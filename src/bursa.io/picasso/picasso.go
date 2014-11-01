fad1e39e462188b1b058578c70c4bffd7bacbb97
derekdowling <thederekdowling@gmail.com>
2014-11-01 11:48:21 -0600

    rock solid picasso tests

--- src/bursa.io/picasso/picasso.go
+++ src/bursa.io/picasso/picasso.go
@@ -12,20 +12,39 @@ import (
 	"runtime"
 )
 
+// Call this to render a template via a Response Writer
 func Render(w http.ResponseWriter, layout string, view string, vars interface{}) {
 
+	template := buildTemplate(layout, view)
+
+	// Provides some visibility into template execution errors.
+	if err := template.Execute(w, vars); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Handles the magic of creating a template
+func buildTemplate(layout string, view string) *template.Template {
+
+	// load our base views folder
 	template_dir := getTemplateRoot()
+
+	// apply expected extensions to the base layout and view
 	layout_path := path.Join(template_dir, layout+".tmpl")
 	view_path := path.Join(template_dir, view+".tmpl")
 
-	// partials := findPartials(layout_path)
+	// collect any partial templates that can be plugged in
+	partials := findPartials(layout_path)
 
-	temp := template.Must(template.ParseFiles(layout_path, view_path))
+	// combine our layout, view, and plugin any partials and return the built template
+	return combineTemplates(layout_path, view_path, partials)
+}
 
-	// Provides some visibility into template execution errors.
-	if err := temp.Execute(w, vars); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+// Plugs in all of our templates to the base layout
+func combineTemplates(layout string, view string, partials []string) *template.Template {
+	templates := []string{layout, view}
+	templates = append(templates, partials...)
+	return template.Must(template.ParseFiles(templates...))
 }
 
 // Creates a relative path to our templates folder
@@ -41,6 +60,7 @@ func getTemplateRoot() string {
 func findPartials(layout_path string) []string {
 
 	expected_partial_dir := path.Join(path.Dir(layout_path), "partials")
+	println(expected_partial_dir)
 
 	// now do a dir listing
 	files, err := filepath.Glob(expected_partial_dir + "/*")
--- src/bursa.io/picasso/picasso_test.go
+++ src/bursa.io/picasso/picasso_test.go
@@ -14,7 +14,7 @@ func TestSpec(t *testing.T) {
 
 	Convey("Picasso Tests", t, func() {
 
-		Convey("getTemplateRoot", func() {
+		Convey("getTemplateRoot()", func() {
 			template_root := getTemplateRoot()
 			So(template_root, ShouldNotBeNil)
 			So(path.Base(template_root), ShouldEqual, "views")
@@ -23,16 +23,18 @@ func TestSpec(t *testing.T) {
 			So(len(files), ShouldBeGreaterThan, 0)
 		})
 
-		Convey("findPartials()", func() {
+		// Used By findPartials() and parseTemplates()
+		config.LoadConfig()
+		template_path := getTemplateRoot()
+		layout := "marketing/layout.tmpl"
+		layout_path := path.Join(template_path, layout)
+		view_path := path.Join(template_path, "marketing/index.tmpl")
 
-			config.LoadConfig()
-			template_path := getTemplateRoot()
-			layout_path := "marketing/layout"
+		Convey("findPartials()", func() {
 
 			Convey("should work with a good path", func() {
 
-				layout := path.Join(template_path, layout_path)
-				partials := findPartials(layout)
+				partials := findPartials(layout_path)
 
 				partial_count := len(partials)
 
@@ -44,12 +46,46 @@ func TestSpec(t *testing.T) {
 				So(signup_index, ShouldNotEqual, partial_count)
 			})
 
-			Convey("not explode on an empty route", func() {
+			Convey("should not explode on an empty route", func() {
 				layout := path.Join(template_path, "notreal/layout")
 				partials := findPartials(layout)
 
 				So(len(partials), ShouldEqual, 0)
 			})
+		})
+
+		Convey("combineTemplates()", func() {
+
+			Convey("should successfully compile a template glob", func() {
+				template := combineTemplates(layout_path, view_path, findPartials(layout_path))
+
+				So(template, ShouldNotBeNil)
+				So(template, ShouldHaveSameTypeAs, template.New("example"))
+				So(template.Lookup("content"), ShouldNotBeNil)
+				So(template.Lookup("signup"), ShouldNotBeNil)
+			})
+
+			Convey("should successfully compile if no partials are present", func() {
+				template := combineTemplates(layout_path, view_path, findPartials("no_partials_path"))
+
+				So(template, ShouldNotBeNil)
+				So(template, ShouldHaveSameTypeAs, template.New("example"))
+				So(template.Lookup("content"), ShouldNotBeNil)
+				So(template.Lookup("signup"), ShouldBeNil)
+			})
+		})
+
+		Convey("buildTemplate()", func() {
+
+			Convey("should successfully build a template", func() {
+				template := buildTemplate("marketing/layout", "marketing/index")
+
+				So(template, ShouldNotBeNil)
+				So(template, ShouldHaveSameTypeAs, template.New("example"))
+				So(template.Lookup("content"), ShouldNotBeNil)
+				So(template.Lookup("signup"), ShouldNotBeNil)
+			})
+
 		})
 	})
 }
