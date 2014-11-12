b5bd1753a5ae5963e847ff674f614deeb1e2a6b4
derekdowling <me@derekdowling.com>
2014-11-12 11:42:41 -0700

    Adding multiple configs via momba to enable travis db testing

---
+++ models/models_test.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestSpec(t *testing.T) {
+
+	Convey("Models Tests", t, func() {
+
+		Convey("should connect", func() {
+			db, err := Connect()
+
+			So(err, ShouldBeNil)
+			So(db, ShouldHaveSameTypeAs, new(gorm.DB))
+		})
+
+	})
+}
