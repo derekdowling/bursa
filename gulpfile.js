07a335b6fa369c32a252fdb9162f3f120e0b64f2
jacobstr <jacobstr@gmail.com>
2014-11-17 08:03:42 -0700

    React and CSS Core.
    Some significant changes. I think duo is completely gone, gulp and watchify
    have taken it's place.
    
    App Changes:
    
    Spiked out some UI components with the idea of having some sort of way to show
    heirarchical wallets. There's currently no "Stores" and "Dispatchers" - important
    react concepts as everything is being done with fixture data.
    
    React router is used to drive rendering. It's quite cool - routes are specified
    in a nested structure, and the path to the deepest valid route determines which
    UI handlers get invoked.
    
    I'm using components for some minimal details right now, like displaying a bitcoin
    hash - that's a component. It made sense because we can add a copy and paste
    behavior to it some time. The balance component (see http://dev.bursa.io/app#/wallets/)
    could do currency conversions based on locale. In general, it feels like there's
    very little overhead and they're a pleasure to write.
    
    Marketing Changes:
    
    - I wanted to make the 404 / Success pages use their empty space better.
      Experimented with flexbox here.
    
      See: http://philipwalton.github.io/solved-by-flexbox/
    - Tweaked the description on the about page.
    - Played with the rendering of the about page. I like the typography on blogs
      like this one: http://robots.thoughtbot.com/the-unix-shells-humble-if. Good
      article btw.
    
    Build Improvements:
    
    - Consolidated transform template differences between the app and the marketing
      site. We're only loading app.css now.
    - Took control of our font build process. It's still kind of a mess but fonts
      tend not to change.
    - `gulp watch` works and is fairly responsive. Incremental rebuilds are handled by
      watchify. All css / js / cjsx changes are reflected in the browser thanks to
      browsersync.
    
      The make this work with vim you want to `set nowritebackup` in your vimrc.
    
      For context:
      http://stackoverflow.com/questions/21608480/gulp-js-watch-task-runs-twice-when-saving-files
    
      There seems to an issue with the css getting minified and losing it's
      sourcemaps. Might have been a rogue gulp process.
    
      - bonus: osx notifications when errors occur. E.g. sass is strict about
        bailing when it finds a syntax error.
    
    - `gulp dist` is like build + additional minification. We might not want our
      sourcemaps leaking until we've figured out our open source strategy.
    - npm shrinkwrapped our deps so that we can specify "*" for dependencies but lock
      down versions.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
-var del = require('del');
-var path = require('path');
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var notify = require('gulp-notify');
+var path = require('path');
+
+// Slightly Stripped Rubix CSS
+
+var path = require('path');
+var gulp = require('gulp');
 
 var flip = require('css-flip');
 var map = require('map-stream');
 var through = require('through');
+var plumber = require('gulp-plumber');
 var transform = require('vinyl-transform');
+var buffer = require('vinyl-buffer');
 var child_process = require('child_process');
+var browserSync = require('browser-sync');
 
 var argv = require('yargs').argv;
 
+var cjsx = require('gulp-cjsx');
+var filter = require('gulp-filter');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 var bless = require('gulp-bless');
 var insert = require('gulp-insert');
@@ -23,7 +34,14 @@ var webpack = require('gulp-webpack');
 var ttf2woff = require('gulp-ttf2woff');
 var cssfont64 = require('gulp-cssfont64');
 var minifycss = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
 var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+
+var watchify = require('watchify');
+var browserify = require('browserify');
+var coffee_reactify = require('coffee-reactify');
+var source = require('vinyl-source-stream');
 
 var runSequence = require('run-sequence');
 
@@ -33,18 +51,36 @@ var production = argv.production ? true : false;
 
 /* file patterns to watch */
 var paths = {
-  // index: ['src/jsx/'+defaultAppName+'/index.html', 'service.js'],
   // l20n: ['src/global/vendor/l20n/*.jsx'],
-  jsx: ['src/jsx/*.jsx', 'src/global/requires/*.js', 'src/jsx/**/*.jsx', 'src/jsx/**/**/*.jsx', 'src/jsx/**/**/**/*.jsx', '!src/global/vendor/l20n/*.jsx', '!src/global/vendor/bootstrap/*.jsx'],
-  scss: ['assets/css/app/**/*.scss', 'assets/css/marketing/**/.*scss'],
-  // bootstrap: ['src/global/vendor/bootstrap/*.jsx'],
-  // ttf: ['public/fonts/dropbox/'+defaultAppName+'/*.ttf']
+  scss: ['assets/css/app/**/*.scss'],
+
+  // Ignore bourbon and bootstrap when watching because they're huge.
+  watch_scss: ['assets/css/app/**/*.scss', '!assets/css/app/bootstrap-sass/**', '!assets/css/app/bourbon/**'],
+
+  js: ['assets/js/**/*.js', 'assets/js/**/*.coffee', 'assets/js/**/*.cjsx'],
+
+  // woff support base64 encoding by default. Otherwise, there's ttf2woff
+  // converters.
+  fonts: [
+    'assets/fonts/data/*',
+    'node_modules/bootstrap/fonts/*',
+    'node_modules/font-awesome/fonts/*'
+  ],
+
+  // Supporting css classes for icons fonts. Glyphicons are part of our bootstrap
+  // build.
+  font_styles: [ 'assets/fonts/styles/*', 'node_modules/font-awesome/scss/font-awesome.scss'],
+
+  // This sets browserify off on it's dependency resolution.
+  entrypoint: './assets/js/app/app.coffee'
 };
 
-var banner = function() {
+// UTILITY
+function banner()
+{
   return '/*! '+package.name+' - v'+package.version+' - '+gutil.date(new Date(), "yyyy-mm-dd")+
           ' [copyright: '+package.copyright+']'+' */';
-};
+}
 
 function logData(data) {
   gutil.log(
@@ -53,103 +89,6 @@ function logData(data) {
     )
   );
 }
-
-logData('Environment : '+ (production ? 'Production':'Development'));
-
-// Loads dependecies via duo
-gulp.task('duo', function() {
-    shell(['duo --use duosass --root assets --copy --output ../static css/vendor.scss js/build.js']);
-});
-
-/* ---------------------------------- */
-/* --------- BEGIN MARKETING:SASS ---------- */
-/* ---------------------------------- */
-gulp.task('sass:marketing', function() {
-  return gulp.src('./assets/css/marketing/main.scss')
-          .pipe(sass({
-            // sourceComments: 'normal' // uncomment when https://github.com/sass/node-sass/issues/337 is fixed
-          }))
-          .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 9'))
-          .pipe(insert.prepend(banner()+'\n'))
-          .pipe(insert.prepend('@charset "UTF-8";\n'))
-          .pipe(gulp.dest('static/css/marketing'));
-});
-
-gulp.task('minifycss:marketing', function() {
-  return gulp.src(['static/css/marketing'])
-          .pipe(minifycss())
-          .pipe(gulp.dest('static/css/marketing'));
-})
-
-gulp.task('bless:marketing', function() {
-  return gulp.src('static/css/marketing/*.css')
-          .pipe(bless())
-          .pipe(insert.prepend(banner()+'\n'))
-          .pipe(insert.prepend('@charset "UTF-8";\n'))
-          .pipe(gulp.dest('static/css/marketing/blessed'));
-});
-
-/* -------------------------------- */
-/* --------- END MARKETING:SASS ---------- */
-/* -------------------------------- */
-
-/* ---------------------------------- */
-/* --------- BEGIN APP:SASS ---------- */
-/* ---------------------------------- */
-gulp.task('sass:app', function() {
-  return gulp.src('./assets/css/app/main.scss')
-          .pipe(sass({
-            // sourceComments: 'normal' // uncomment when https://github.com/sass/node-sass/issues/337 is fixed
-          }))
-          .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 9'))
-          .pipe(insert.prepend(banner()+'\n'))
-          .pipe(insert.prepend('@charset "UTF-8";\n'))
-          .pipe(gulp.dest('static/css/app'));
-});
-
-gulp.task('minifycss:app', function() {
-  return gulp.src(['static/css/app'])
-          .pipe(minifycss())
-          .pipe(gulp.dest('static/css/app'));
-});
-
-gulp.task('bless:app', function() {
-  return gulp.src('static/css/app/*.css')
-          .pipe(bless())
-          .pipe(insert.prepend(banner()+'\n'))
-          .pipe(insert.prepend('@charset "UTF-8";\n'))
-          .pipe(gulp.dest('static/css/app/blessed'));
-});
-
-/* -------------------------------- */
-/* --------- END APP:SASS ---------- */
-/* -------------------------------- */
-
-/* ------------------------------ */
-/* --------- GULP TASKS --------- */
-/* ------------------------------ */
-gulp.task('build:app', ['duo', 'sass:app']);
-gulp.task('build:marketing', ['duo', 'sass:marketing']);
-
-//This doesn't use the above tasks to avoid running the duo
-//command multiple times per build
-gulp.task('build', ['duo', 'sass:app', 'sass:marketing']);
-
-// For Production Builds
-gulp.task('bless', ['bless:app', 'bless:marketing']);
-gulp.task('minifycss', ['minifycss:app', 'minifycss:marketing']);
-gulp.task('build:dist', ['minifycss', 'bless']);
-
-/*BEGIN: ALIASES FOR CERTAIN TASKS (for Watch)*/
-gulp.task('build:app:watch', ['build:app'], ready);
-gulp.task('build:css:watch', ['build:css'], ready);
-gulp.task('react-bootstrap:watch', ['react-bootstrap'], ready);
-/*END: ALIASES*/
-
-gulp.task('watch', function() {
-  // gulp.watch(paths.jsx, ['build:app:watch']);
-  gulp.watch(paths.scss, ['rebuild:css']);
-});
 
 function ready() {
   gutil.log(
@@ -160,3 +99,177 @@ function ready() {
     )
   );
 }
+
+logData('Environment : '+ (production ? 'Production':'Development'));
+
+// Javascript Related
+// -----------
+
+function bundlefile(file, as, watch) {
+  var props = watchify.args;
+  props.entries = [file];
+  props.debug = true;
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+  bundler.transform(coffee_reactify);
+
+
+  var onError = function(err) {
+    console.log(err);
+    notify.onError({
+      title:    "Sass",
+      subtitle: "Failure!",
+      message:  "Error: <%= err.message %>"
+    })(err);
+
+    this.emit('end');
+  };
+
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream
+      .pipe(plumber({errorHandler: onError}))
+      .on('error', gutil.log.bind(gutil, 'error'))
+      .pipe(source(as))
+      .pipe(gulp.dest('static/js'))
+      .pipe(browserSync.reload({stream:true, once: true}));
+  }
+
+  bundler.on('update', function() {
+    rebundle();
+    gutil.log('Rebundle...');
+  })
+  .on('exit', function() {
+    gutil.log('Done...');
+  });
+
+  return rebundle();
+}
+
+gulp.task('js:app', function() {
+  return bundlefile(paths.entrypoint, 'app.js', false);
+});
+
+gulp.task('js:app:watch', function() {
+  return bundlefile(paths.entrypoint, 'app.js', true);
+});
+
+gulp.task('uglify', function() {
+  return gulp.src('static/js/app.js')
+  .pipe(uglify('build.min.js', {
+    preserveComments: false,
+    compress: {
+      warnings: false
+    }
+  }))
+  .pipe(gulp.dest('static/js'));
+});
+
+// CSS Related
+// -----------
+
+function sassify(src, alias, dest) {
+  return function() {
+    return gulp.src(src)
+      .pipe(sourcemaps.init())
+      .pipe(
+        sass({
+          errLogToConsole: true,
+          sourceComments: 'map',
+          onError: notify.onError({
+            title: "Compile Error",
+            message: "<%= error.message %>"
+          })
+      }))
+      // .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 9'))
+      .pipe(sourcemaps.write())
+      .pipe(rename(alias))
+      .pipe(gulp.dest(dest))
+      .pipe(browserSync.reload({stream:true}));
+    };
+}
+
+gulp.task('app:sass', sassify('./assets/css/app/main.scss', 'app.css', 'static/css'));
+
+gulp.task('minifycss', function() {
+  gutil.log("running minfiy?");
+  return gulp.src(['static/css/*.css'])
+    .pipe(minifycss())
+    .pipe(gulp.dest('static/css'));
+});
+
+gulp.task('bless', function() {
+  return gulp.src('static/css/*.css')
+    .pipe(bless())
+    .pipe(insert.prepend('@charset "UTF-8";\n'))
+    .pipe(gulp.dest('static/css/app/blessed'));
+});
+
+// Compile sass in parallel.
+gulp.task('sass', ['app:sass']);
+
+// gulp.task('bless', ['app:bless']);
+
+gulp.task('build:css', ['fonts', 'sass']);
+
+gulp.task('build:js', ['js:app']);
+gulp.task('build', ['build:css', 'build:js']);
+
+// Fonts
+// -----
+
+gulp.task('fonts:clean', function() {
+  return gulp.src('.build/css/fonts/*', {read: false}).pipe(clean());
+});
+
+// We base64 encode our fonts.
+gulp.task('fonts:copy', ['fonts:clean'], function() {
+  return gulp.src(paths.fonts)
+  .pipe(gulp.dest('static/fonts'));
+});
+
+// Corresponding font defintions e.g. .fa-circle-plus
+gulp.task('fonts:sass', ['fonts:clean'], sassify(paths.font_styles, 'fonts.css', '.build/css/fonts'));
+gulp.task('fonts:css', ['fonts:clean'], function() {
+  return gulp
+    .src(paths.font_styles)
+    .pipe(filter('*.css'))
+    .pipe(gulp.dest('.build/css/fonts'));
+});
+
+gulp.task('fonts:concat', ['fonts:sass', 'fonts:css'], function(){
+  return gulp
+    .src('.build/css/fonts/*')
+    .pipe(concat('fonts.css'))
+    .pipe(gulp.dest('static/css'));
+});
+
+// Combine font definitions and font data into a single file.
+gulp.task('fonts', ['fonts:copy', 'fonts:concat']);
+
+gulp.task('dist', function(cb) {
+  runSequence('build', 'minifycss', 'uglify', function() {
+    cb();
+    gutil.log(gutil.colors.green("Build complete"));
+  });
+});
+
+// Watch Tasks
+//------------
+
+gulp.task('build:css:watch', ['sass'], ready);
+gulp.task('build:js:watch', ['js:app:watch'], ready);
+gulp.task('react-bootstrap:watch', ['react-bootstrap'], ready);
+
+gulp.task('watch:css', function() {
+  gulp.watch(paths.watch_scss, ['build:css:watch']);
+});
+
+gulp.task('watch', ['watch:css', 'build:js:watch', 'browser-sync']);
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    proxy: "localhost:8080",
+    debug: true
+  });
+});
